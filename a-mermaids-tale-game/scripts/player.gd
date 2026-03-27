dfb55bce2740860033ab4308523932f139997241
mnurbol-glitch <mnurbol@fordham.edu>
2026-03-26 22:32:31 -0400

    2nd scene update 1

--- a-mermaids-tale-game/scripts/player.gd
+++ a-mermaids-tale-game/scripts/player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+@export var speed = 600
+
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
-	print(direction)
+	velocity = direction  * speed
+	move_and_slide()
+	
