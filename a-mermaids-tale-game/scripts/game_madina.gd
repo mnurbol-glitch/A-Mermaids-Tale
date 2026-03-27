2f29a2c9044fc94c65476c6d3f6f38dcfed3f4e5
mnurbol-glitch <mnurbol@fordham.edu>
2026-03-26 23:51:52 -0400

    error

--- a-mermaids-tale-game/scripts/game_madina.gd
+++ a-mermaids-tale-game/scripts/game_madina.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var player_spawn_pos = $PlayerSpawnPos
+@onready var trident_container = $TridentContainer
 
 var player = null
 
@@ -8,12 +9,15 @@ func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	assert(player!=null)
 	player.global_position = player_spawn_pos.global_position
-	
+	player.trident_shot.connect(_on_player_trident_shot)
 	
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
-		
-		
+
+func _on_player_trident_shot(trident_scene, location):
+	var trident = trident_scene.instatiate()
+	trident.global_position = location
+	trident_container.add_child(trident)
