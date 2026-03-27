c001016cfffd387961338d9c8db0b31b8ea93744
mnurbol-glitch <mnurbol@fordham.edu>
2026-03-26 23:39:56 -0400

    2nd scene update 3

--- a-mermaids-tale-game/scripts/player.gd
+++ a-mermaids-tale-game/scripts/player.gd
@@ -1,9 +1,21 @@
 extends CharacterBody2D
 
+signal trident_shot(trident_scene, location)
+
 @export var speed = 600
+
+@onready var muzzle = $Muzzle
+
+var trident_scene = preload("res://scenes/trident.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	velocity = direction  * speed
 	move_and_slide()
 	
+func shoot():
+	trident_shot.emit(trident_scene, muzzle.global_position)
--- a-mermaids-tale-game/scripts/trident.gd
+++ a-mermaids-tale-game/scripts/trident.gd
@@ -1,11 +1,9 @@
 extends Area2D
 
+@export var speed = 1000
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	global_position.y += -speed * delta
+	
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
